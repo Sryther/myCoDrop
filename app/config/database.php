78c578ef834209cbc039cd73f96c892ffede4c74
Paul Rey <paul.rey@ingesup.com>
2013-10-30 14:56:10 +0100

    Changement du nom de base de donnees et identifiants de mySQL

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'myDropCode',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',

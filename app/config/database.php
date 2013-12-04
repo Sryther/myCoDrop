a023b6428e22f3a91f93176c96bd13794818a5a1
Aneley <yelena.pooda@ingesup.com>
2013-12-04 14:32:33 +0100

    identifiant & mdp

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'fravimal.me',
+			'host'      => '88.191.164.186',
 			'database'  => 'mycodrop',
 			'username'  => 'mycodrop',
 			'password'  => 'FQEqQKF6YzMfBxrY',
